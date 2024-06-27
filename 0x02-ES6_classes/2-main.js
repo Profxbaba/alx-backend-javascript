d9d75c8a12e1e0fbbecb1aafa5ab8b1fde3393e4
Profxbaba <daddyslove40@gmail.com>
2024-06-27 23:20:28 +0000

    2 main

--- 0x02-ES6_classes/2-main.js
+++ 0x02-ES6_classes/2-main.js
@@ -1,28 +1,20 @@
 import HolbertonCourse from "./2-hbtn_course.js";
 
+const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"])
+console.log(c1.name);
+c1.name = "Python 101";
+console.log(c1);
+
 try {
-    const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"]);
-    console.log(c1.name);
-    c1.name = "Python 101";
-    console.log(c1);
+    c1.name = 12;
+} 
+catch(err) {
+    console.log(err);
+}
 
-    try {
-        c1.name = 12; // This will throw a TypeError
-    } catch (err) {
-        console.log(err.message);
-    }
-
-    try {
-        c1.length = "1"; // This will throw a TypeError
-    } catch (err) {
-        console.log(err.message);
-    }
-
-    try {
-        const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]); // This will throw a TypeError
-    } catch (err) {
-        console.log(err.message);
-    }
-} catch (err) {
-    console.log(err.message);
+try {
+    const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]);
+}
+catch(err) {
+    console.log(err);
 }
