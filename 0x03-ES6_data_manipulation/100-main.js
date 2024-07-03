587085d65cd545fb68d7103dc0f6c5f95ff96c8e
Profxbaba <daddyslove40@gmail.com>
2024-07-03 22:15:28 +0000

    ES6 data manipulation

--- 0x03-ES6_data_manipulation/100-main.js
+++ 0x03-ES6_data_manipulation/100-main.js
@@ -1,22 +1,15 @@
 import { queryAPI, weakMap } from "./100-weak.js";
 
 const endpoint = { protocol: 'http', name: 'getUsers' };
+weakMap.get(endpoint);
 
+queryAPI(endpoint);
 console.log(weakMap.get(endpoint));
 
-try {
-  queryAPI(endpoint);
-  console.log(weakMap.get(endpoint));
+queryAPI(endpoint);
+console.log(weakMap.get(endpoint));
 
-  queryAPI(endpoint);
-  console.log(weakMap.get(endpoint));
-
-  queryAPI(endpoint);
-  queryAPI(endpoint);
-  queryAPI(endpoint);
-  queryAPI(endpoint); // This will throw an error
-} catch (error) {
-  console.error(error.message);
-}
-
-console.log(weakMap.get(endpoint)); // This won't execute due to the error
+queryAPI(endpoint);
+queryAPI(endpoint);
+queryAPI(endpoint);
+queryAPI(endpoint);
